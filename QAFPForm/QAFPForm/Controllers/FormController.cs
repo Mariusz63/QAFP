a19e45db0d3b8ea34c891b770f59c36c639b50d6
Mariusz63 <76663831+Mariusz63@users.noreply.github.com>
2025-11-27 18:00:51 +0100

    działa ale do poprawy
    zle działa zapis zml, poprawic wyglad, wyglad do drku w miare poprawny

--- QAFPForm/QAFPForm/Controllers/FormController.cs
+++ QAFPForm/QAFPForm/Controllers/FormController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QAFPForm.Models;
-using System.Diagnostics;
+using System.Xml.Linq; 
+using System.IO;
 
 namespace QAFPForm.Controllers
 {
@@ -42,6 +43,9 @@ namespace QAFPForm.Controllers
             model.Id = _store.Count + 1;
             _store.Add(model);
 
+            // zapis do XML
+            SaveToXml(model);
+
             return RedirectToAction("Success", new { id = model.Id });
         }
 
@@ -53,13 +57,77 @@ namespace QAFPForm.Controllers
             return View(model);
         }
 
+        private void SaveToXml(FormSubmission model)
+        {
+            // katalog "Data" w katalogu głównym projektu (bez wwwroot)
+            string dataFolder = Path.Combine(Directory.GetCurrentDirectory(), "Data");
+            // Tworzymy katalog jeśli nie istnieje
+            if (!Directory.Exists(dataFolder))
+            {
+                Directory.CreateDirectory(dataFolder);
+            }
+            string filePath = Path.Combine(dataFolder, "submissions.xml");
+
+            XDocument doc;
+
+            if (System.IO.File.Exists(filePath))
+            {
+                doc = XDocument.Load(filePath);
+            }
+            else
+            {
+                doc = new XDocument(new XElement("Submissions"));
+            }
+
+            XElement submissionElement = new XElement("Submission",
+                new XElement("Id", model.Id),
+                new XElement("Nazwisko", model.Nazwisko),
+                new XElement("Imie", model.Imie),
+                new XElement("Telefon", model.Telefon),
+                new XElement("Email", model.Email),
+                new XElement("Pesel", model.Pesel),
+                new XElement("Regon", model.Regon),
+                new XElement("Nip", model.Nip),
+                new XElement("Siedziba", model.Siedziba),
+                new XElement("Zaklad", model.Zaklad),
+                new XElement("Korespondencja", model.Korespondencja),
+                new XElement("ProdukcjaZwierzeca", model.ProdukcjaZwierzeca),
+                new XElement("Uboj", model.Uboj),
+                new XElement("Rozbior", model.Rozbior),
+                new XElement("Przetworstwo", model.Przetworstwo),
+                new XElement("ProdukcjaKonserw", model.ProdukcjaKonserw),
+                new XElement("Dystrybucja", model.Dystrybucja),
+                new XElement("ZakresProdukcji", model.ZakresProdukcji),
+                new XElement("ProcentProdukcji", model.ProcentProdukcji),
+                new XElement("Obiekty", model.Obiekty),
+                new XElement("Produkcyjne", model.Produkcyjne),
+                new XElement("Magazynowe", model.Magazynowe),
+                new XElement("Oczyszczalnie", model.Oczyszczalnie),
+                new XElement("Odpady", model.Odpady),
+                new XElement("Dzialki", model.Dzialki),
+                new XElement("CalkowitaPow", model.CalkowitaPow),
+                new XElement("PowierzchniaUR", model.PowierzchniaUR),
+                new XElement("Podzlecanie", model.Podzlecanie),
+                new XElement("RodzajPodzlecanych", model.RodzajPodzlecanych),
+                new XElement("Transport", model.Transport),
+                new XElement("OpisProcesu", model.OpisProcesu),
+                new XElement("JednostkaCertyfikujaca", model.JednostkaCertyfikujaca),
+                new XElement("Zal_DokumentyStatus", model.Zal_DokumentyStatus),
+                new XElement("Zal_PlanySytuacyjne", model.Zal_PlanySytuacyjne),
+                new XElement("Zal_PlanyProdukcyjne", model.Zal_PlanyProdukcyjne),
+                new XElement("Zal_SkladProduktu", model.Zal_SkladProduktu)
+            );
+
+            doc.Root!.Add(submissionElement);
+            doc.Save(filePath);
+        }
+
+
         // Lista dla Admina
         [HttpGet]
         public IActionResult List()
         {
             return View(_store);
         }
-
-        
     }
 }
